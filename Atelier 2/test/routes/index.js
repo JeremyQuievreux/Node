f313634f517941cc6506e1ef3be1cb84d776aa1c
DevHÿllen <64634619+Jequi59@users.noreply.github.com>
2021-10-04 14:19:10 +0200

    123

--- Atelier 2/test/routes/index.js
+++ Atelier 2/test/routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [{
+  name: "Jeremy",
+  age: 36,
+  isAdmin : true
+  },{
+    name:"jeff",
+    age: 39,
+    isAdmin: false
+  },{
+    name:"lois",
+    age:24,
+    isAdmin : false
+  },{
+    name:"narek",
+    age: 25,
+    isAdmin : false
+  }]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
 });
+router.get('/mypage/', function(req, res, next) {
+  res.render('page1', { title: 'Présentation' , nom: "Quievreux", prenom: "Jeremy", email: "monmail@gmail.com"});
+});
+
+/* router.get('/:prenom', function(req, res, next) {
+  let prenom = req.params.prenom;
+  res.render('autre', { pseudo: prenom});
+}); */
+
+
 
 module.exports = router;
--- Atelier 2/test/routes/users.js
+++ Atelier 2/test/routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [{
+  name: "Jeremy",
+  age: 36,
+  isAdmin : true
+  },{
+    name:"jeff",
+    age: 39,
+    isAdmin: false
+  },{
+    name:"lois",
+    age:24,
+    isAdmin : false
+  },{
+    name:"narek",
+    age: 25,
+    isAdmin : false
+  }]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { data: users});
 });
 
 module.exports = router;
